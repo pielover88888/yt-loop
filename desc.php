20d299d5909e13d5b4ec0b0945359180e5955a98
Jeff <pielover88888@live.com>
2015-06-28 01:37:07 -0400

    first commit

---
+++ desc.php
@@ -0,0 +1,10 @@
+<form action="" method="GET">
+<input name="url" type="text" placeholder="YouTube URL" value="<?php echo $url; ?>">
+<input type="submit" value="Embed">
+</form>
+<B>hello.</b> Enter a youtube address to have it embedded via direct mp4 link.
+<s><strike>currently,
+playlist urls dont work or play at all, so remove the &list=..blah,
+k?
+As of 12:17, june 28th, 2015 i am working on autoremoving it
+server side.</strike></s> Fixed, 12:21
---
+++ index.php
@@ -0,0 +1,40 @@
+<style>
+<?php
+echo file_get_contents('../teewo/normalize.css');
+echo file_get_contents('../teewo/skeleton.css');
+?>
+
+</style>
+
+<?php
+include "../flhead.php";
+if(isset($_GET["url"]) ){
+$url = $_GET["url"];
+$url = explode("&list",$url)[0];
+shell_exec("rm file.mp4" . " > tel.txt");
+
+$command = "youtube-dl -g -f 18 " . escapeshellcmd($url);
+$theURL = shell_exec($command . "");
+//sleep(4);
+}
+//https://www.youtube.com/watch?v=uw_6_7CRwAs
+$rand = rand(1,1000) + "=" + rand(1,1000);
+?>
+<div id="info" style="position:absolute;left:1em; width:400px;">
+</div>
+
+<?php if(isset($_GET["url"])) : ?>
+<video src="<?= $theURL ?>" controls loop width=720 style="float:right;margin-right:1em;" autoplay autoload>
+<?php else : ?>
+
+
+<?php endif; ?>
+<span id="real" style="display:none;">
+<?php include "desc.php"; ?>
+</span>
+<script>
+var info = document.getElementById("info").innerHTML = document.getElementById("real").innerHTML;
+//info.innerHTML = "hi";
+var thing = "<?php echo htmlspecialchars($url); ?>"
+var inf = document.getElementById("input").innerHTML = thing;
+</script>
